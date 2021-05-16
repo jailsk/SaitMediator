d6bbe43ece4d1809aa69d6cfb1e63983403dfe9b
Jails <jailsk>
2021-05-16 15:08:37 +0530

    loginpage

--- app/src/main/java/com/devSait/saitMediator/MainActivity.java
+++ app/src/main/java/com/devSait/saitMediator/MainActivity.java
@@ -2,13 +2,30 @@ package com.devSait.saitMediator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button msignupbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        msignupbtn = findViewById(R.id.signupbtn);
+        msignupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register_page();
+            }
+        });
+    }
+    public void register_page(){
+        Intent intent = new Intent(this, register_page.class);
+        startActivity(intent);
+
     }
 }
