f6253500d0411ed00c1abe8135f1d230bbf8e42d
Sudeep Richard Baxla <srb7309264849@gmail.com>
2021-05-16 19:24:14 +0530

    login page

--- app/src/main/java/com/devSait/saitMediator/login_page.java
+++ app/src/main/java/com/devSait/saitMediator/login_page.java
@@ -6,13 +6,13 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class login_page extends AppCompatActivity {
-    Button register;
+    //Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
-        register=findViewById(R.id.registerbtn2);
+       // register=findViewById(R.id.registerbtn2);
 
     }
 }
