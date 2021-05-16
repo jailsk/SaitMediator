356eb9f5a5f1acc0b8870a18005fce9263264057
Sudeep Richard Baxla <srb7309264849@gmail.com>
2021-05-16 18:12:42 +0530

    Splash

--- app/src/main/java/com/devSait/saitMediator/MainActivity.java
+++ app/src/main/java/com/devSait/saitMediator/MainActivity.java
@@ -1,31 +1,44 @@
 package com.devSait.saitMediator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button msignupbtn;
+
+    private static final int SPLASH_SCREEN = 3550;
+    //variables
+    Animation topanim,bottomanim;
+    ImageView logo;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        msignupbtn = findViewById(R.id.signupbtn);
-        msignupbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                register_page();
-            }
-        });
-    }
-    public void register_page(){
-        Intent intent = new Intent(this, register_page.class);
-        startActivity(intent);
+    //Animation
+    topanim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+    bottomanim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
+    logo=findViewById(R.id.imageView);
+    text=findViewById(R.id.textView2);
+
+    logo.setAnimation(topanim);
+    text.setAnimation(bottomanim);
+
+    new Handler().postDelayed(() -> {
+        Intent intent=new Intent(MainActivity.this,login_page.class);
+        startActivity(intent);
+        finish();
+    },SPLASH_SCREEN);
     }
 }
--- app/src/main/java/com/devSait/saitMediator/login_page.java
+++ app/src/main/java/com/devSait/saitMediator/login_page.java
@@ -3,12 +3,16 @@ package com.devSait.saitMediator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class login_page extends AppCompatActivity {
-
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        register=findViewById(R.id.registerbtn2);
+
     }
 }
