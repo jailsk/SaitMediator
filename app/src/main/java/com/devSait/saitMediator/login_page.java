08d4c7ce404031656222ae17ef0ae3576433f472
Jails <jailsk>
2021-05-17 19:36:34 +0530

    Login page added.

--- app/src/main/java/com/devSait/saitMediator/login_page.java
+++ app/src/main/java/com/devSait/saitMediator/login_page.java
@@ -2,12 +2,9 @@ package com.devSait.saitMediator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
 import android.os.Bundle;
-import android.util.Pair;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.view.View;
 import android.content.Intent;
 
 public class login_page extends AppCompatActivity {
@@ -21,7 +18,7 @@ public class login_page extends AppCompatActivity {
         Button register=(Button)findViewById(R.id.registerbtn2);
         register.setOnClickListener(v -> {
             // TODO Auto-generated method stub
-            Intent registration=new Intent(getApplicationContext(),register_page.class);
+            Intent registration=new Intent(getApplicationContext(), register_page.class);
             startActivity(registration);
         });
 
--- app/src/main/java/com/devSait/saitMediator/register_page.java
+++ app/src/main/java/com/devSait/saitMediator/register_page.java
@@ -1,87 +1,157 @@
 package com.devSait.saitMediator;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class register_page extends AppCompatActivity {
-    EditText mrollno,memail,mpassword1,mpassword2;
-    Button mregisterbtn;
+    public static final String TAG = "TAG";
+    TextInputLayout mrollno,mEmail,mPassword1,mPassword2;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
     FirebaseAuth fAuth;
-    ProgressBar mprogressBar;
-
-
-
-
-
+    ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
-        mrollno     = findViewById(R.id.rollno);
-        memail      = findViewById(R.id.email);
-        mpassword1  =  findViewById(R.id.password1);
-        mpassword2  =   findViewById(R.id.password2);
-        mregisterbtn =  findViewById(R.id.registerbtn);
+        mrollno   = findViewById(R.id.rollno);
+        mEmail      = findViewById(R.id.email);
+        mPassword1   = findViewById(R.id.password1);
+        mPassword2      = findViewById(R.id.password2);
+        mRegisterBtn= findViewById(R.id.registerbtn);
+        mLoginBtn   = findViewById(R.id.loginbtn);
 
         fAuth = FirebaseAuth.getInstance();
-        mprogressBar = findViewById(R.id.progressBar);
+        fStore = FirebaseFirestore.getInstance();
+        progressBar = findViewById(R.id.progressBar);
 
-        if (fAuth.getCurrentUser() != null){
+        if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
         }
+        mLoginBtn.setOnClickListener(v -> {
+            Intent i=new Intent(getApplicationContext(),login_page.class);
+            startActivity(i);
+        });
 
-        mregisterbtn.setOnClickListener(new View.OnClickListener() {
+
+        mRegisterBtn.setOnClickListener(v -> {
+            final String email = Objects.requireNonNull(mEmail.getEditText()).getText().toString().trim();
+            String password1 = Objects.requireNonNull(mPassword1.getEditText()).getText().toString().trim();
+            final String rollnum = Objects.requireNonNull(mrollno.getEditText()).getText().toString();
+            final String password2    = Objects.requireNonNull(mPassword2.getEditText()).getText().toString();
+
+            if(TextUtils.isEmpty(email)){
+                mEmail.setError("Email is Required.");
+                return;
+            }
+
+            if(TextUtils.isEmpty(password1)){
+                mPassword1.setError("Password is Required.");
+                return;
+            }
+            if(TextUtils.isEmpty(password2)){
+                mPassword2.setError("Please retype your password.");
+                return;
+            }
+            if(password1.equals(password2)){
+                mPassword2.setError("Password is not same.");
+                return;
+            }
+
+            if(password1.length() < 6){
+                mPassword1.setError("Password Must be >= 6 Characters");
+                return;
+            }
+
+            progressBar.setVisibility(View.VISIBLE);
+
+            // register the user in firebase
+
+            fAuth.createUserWithEmailAndPassword(email,password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+
+                        // send verification link
+
+                        FirebaseUser fuser = fAuth.getCurrentUser();
+                        fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(register_page.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure: Email not sent " + e.getMessage());
+                            }
+                        });
+
+                        Toast.makeText(register_page.this, "User Created.", Toast.LENGTH_SHORT).show();
+                        userID = fAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fStore.collection("users").document(userID);
+                        Map<String,Object> user = new HashMap<>();
+                        user.put("rollno",rollnum);
+                        user.put("email",email);
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "onSuccess: user Profile is created for "+ userID);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure: " + e.toString());
+                            }
+                        });
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                    }else {
+                        Toast.makeText(register_page.this, "Error ! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }
+            });
+        });
+
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = memail.getText().toString().trim();
-                String password1 = mpassword1.getText().toString().trim();
-
-                if (TextUtils.isEmpty(email)) {
-                    memail.setError("Email required");
-                    return;
-                }
-                if (TextUtils.isEmpty(password1)) {
-                    memail.setError("password required");
-                    return;
-                }
-                if (password1.length() < 6) {
-                    mpassword1.setError("password more than 6 ch");
-                    return;
-                }
-                mprogressBar.setVisibility(View.VISIBLE);
-
-                fAuth.createUserWithEmailAndPassword(email, password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(register_page.this, "user created.", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        } else {
-                            Toast.makeText(register_page.this, "error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                });
+                startActivity(new Intent(getApplicationContext(),Dashboard.class));
             }
         });
+
     }
 }
-
