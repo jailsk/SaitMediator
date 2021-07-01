260b276c3f122b0fc3a35f9426f65ed64ba6a311
Sudeep Richard Baxla <srb7309264849@gmail.com>
2021-07-01 21:08:01 +0530

    Dashboard  created

--- app/src/main/java/com/devSait/saitMediator/Dashboard.java
+++ app/src/main/java/com/devSait/saitMediator/Dashboard.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -16,6 +18,12 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ImageView profile_pic;
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        FirebaseAuth fAuth= FirebaseAuth.getInstance();
+
+
+
 
     }
         public void logout(View view){
